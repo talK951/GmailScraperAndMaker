25d9b092b7570f131f6b2d35d9089059ab6afedb
talK951 <49502747+talK951@users.noreply.github.com>
2024-05-09 17:03:57 -0400

    KAN-0-Initial setup commit (Added table by accident)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { ClientTable } from './ClientTable/ClientTable.js'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ClientTable></ClientTable>
       </header>
+      
     </div>
   );
 }
