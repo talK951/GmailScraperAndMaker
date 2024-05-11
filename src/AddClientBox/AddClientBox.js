6455c1914ee2756767ea8d10ad1258f0d573da18
Sean-Mcleod <Sean.mcleod@mths.ca>
2024-05-11 13:08:21 -0400

    KAN-8-basic-styling

--- src/AddClientBox/AddClientBox.js
+++ src/AddClientBox/AddClientBox.js
@@ -3,18 +3,21 @@ import './AddClientBox.css';
 const AddClientBox = () => {
     return ( 
         <div className="box">
-            <div className="name">
-                Name: <input type="text" className="textbox" />
-            </div>
-            <div className="email">
-                Email: <input type="text" className="textbox" />
-            </div>
-            <div className="information">
-                Information: <input type="text" className="textbox" />
-            </div>
-            <button type="button">Upload Files</button>
-            <button type="button">Add Content</button>
+            <div className="container">
+                <div className="grid-item-1">
+                    <div>Name: <input type="text" className="textbox" /></div>
+                    <div>Email: <input type="text" className="textbox" /></div>
+                </div>
 
+                <div className="information">
+                    Information: <input type="text" className="textbox" />
+                </div>
+
+                <div className="buttondiv">
+                    <button type="button">Upload Files</button>
+                    <button type="button">Add Content</button>
+                </div>
+            </div>
         </div>
      );
 }
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ClientTable } from './ClientTable/ClientTable.js'
 import { EmailTable } from './EmailTable/EmailTable.js'
+import AddClientBox from './AddClientBox/AddClientBox.js';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <ClientTable />
         <EmailTable />
+        <AddClientBox />
       </header>
       
     </div>
