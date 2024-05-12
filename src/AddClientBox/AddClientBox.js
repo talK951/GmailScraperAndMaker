ad0507fcf763ea65c7e3cb03090a7a048d5f793f
Sean-Mcleod <Sean.mcleod@mths.ca>
2024-05-11 22:24:45 -0400

    KAN-8-complete add client functionality

--- src/AddClientBox/AddClientBox.js
+++ src/AddClientBox/AddClientBox.js
@@ -1,21 +1,50 @@
+import { useState } from 'react';
 import './AddClientBox.css';
 
+let idCount = 0;
 const AddClientBox = () => {
-    return ( 
+    const [clients, setClients] = useState([]);
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [info, setInfo] = useState("");
+
+    const addClient = (name, email, info) => {
+        setClients(
+            [
+                ...clients,
+                { id: idCount++, name: name, email: email, info: info} // and one new item at the end
+            ]
+        );
+    }
+
+    return (
         <div className="box">
             <div className="container">
                 <div className="grid-item-1">
-                    <div>Name: <input type="text" className="textbox" /></div>
-                    <div>Email: <input type="text" className="textbox" /></div>
+                    <div>Name: <input type="text" className="textbox" onChange={(evt) => { setName(evt.target.value) }}/></div>
+                    <div>Email: <input type="text" className="textbox" onChange={(evt) => { setEmail(evt.target.value) }}/></div>
                 </div>
 
                 <div className="information">
-                    Information: <input type="text" className="textbox" />
+                    Information: <input type="text" className="textbox" onChange={(evt) => { setInfo(evt.target.value) }}/>
                 </div>
 
-                <div className="buttondiv">
+                {/* print clients */}
+                {/* {clients.map((item) =>(
+                    <ul>
+                        <li>{item.id}</li>
+                        <li>{item.name}</li>
+                        <li>{item.email}</li>
+                        <li>{item.info}</li>
+                    </ul>
+                ))
+                } */}
+
+
+                <div className="button-div">
                     <button type="button">Upload Files</button>
-                    <button type="button">Add Content</button>
+                    <button type="button" onClick={() => addClient(name, email, info)}>Add Content</button>
                 </div>
             </div>
         </div>
